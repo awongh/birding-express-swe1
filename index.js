a719b5a3cd90b1f5a3c656265cca54048a83e5a3
awongh <akira@awongh.com>
2020-11-26 13:59:37 +0800

    basic app

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('hello world');
+import pg from 'pg';
+const { Pool } = pg;
+
+const pgConnectionConfigs = {
+  user: process.env.db_user,
+  host: 'localhost',
+  database: 'recipes',
+  port: 5432, // Postgres server always runs on this port
+};
+
+const pool = new Pool(pgConnectionConfigs);
+
+import express from 'express';
+
+const app = express();
+
+const PORT = process.argv[2];
+
+app.get('/recipe/:id', (request, response) => {
+
+  const values = [request.params.id];
+
+  pool.query('select * from recipes WHERE id=$1', values, (error, result)=>{
+
+    if( error ){
+      console.log( error );
+      response.status(503).send('error');
+      return;
+    }
+
+    response.send( result.rows );
+  });
+
+});
+
+app.listen(PORT);
---
+++ seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO recipes (name) VALUES ('udon');
+INSERT INTO recipes (name) VALUES ('mee pok');
+INSERT INTO recipes (name) VALUES ('pasta');
